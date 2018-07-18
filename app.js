7e2550ed26d122fa3ee687372295e2e9e395365a
Athul Muralidharan <athulmurali@gmail.com>
2018-07-18 19:10:17 +0530

    added passport support

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var logger = require('morgan')
 
 var User     = require('./models/User');;
 
+var authRouter = require('./routes/auth');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
